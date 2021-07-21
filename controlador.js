ac5d522bfd7c990513616062e2e50644367de048
VicRaji <72216830+VicRaji@users.noreply.github.com>
2021-07-21 16:53:02 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,10 @@
+ /*Programa que nos ayude a establecer el costo total de una consola de video juegos*/
+ /* -En el exterior
+    -Compra en dolares
+    -Pagar un casillero
+    -(dolares costo dependiendo de la cantidad de productos)
+    -envío hasta colombia
+    -(cantidad de productos)
+    -pagar impuesto de entrada al pais (lo digita aduanas)
+    */
+   
